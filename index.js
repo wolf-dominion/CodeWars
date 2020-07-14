93a4f65692048e175af1c058e56fff81f17ca923
wolf-dominion <hannah_drury@yahoo.com>
2020-07-14 17:48:48 -0600

    remove wub from strings to return the sentence

--- index.js
+++ index.js
@@ -29,3 +29,24 @@ function DNAStrand(dna){
     }
     return complement.join("")
   }
+
+//   Write a function that takes an integer as input, 
+//   and returns the number of bits that are equal to one 
+//   in the binary representation of that number. 
+//   You can guarantee that input is non-negative.
+
+//   Example: The binary representation of 
+//   1234 is 10011010010, so the function should return 5 
+//   in this case
+
+  var countBits = function(n) {
+    // Program Me
+  };
+
+  // remove wub from strings to return the sentence
+
+  function songDecoder(song){
+    song = song.replace(/WUB/g, " ")
+    return song.replace(/\s+/g,' ').trim();
+
+  }
