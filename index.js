e98c829833cbc6306ea4dae26c666be79177e4e3
wolf-dominion <hannahdruryutd@gmail.com>
2020-07-27 16:14:37 -0600

    function for each number and operator

--- index.js
+++ index.js
@@ -68,3 +68,52 @@ function DNAStrand(dna){
 
     return strArray.join(" ")
   }
+
+// There must be a function for each number from 0 (“zero”) to 9 (“nine”)
+// There must be a function for each of the following mathematical operations:
+
+// plus, minus, times, dividedBy (divided_by in Ruby)
+
+// Each calculation consist of exactly one operation and two numbers
+// The most outer function represents the left operand, the most inner function represents the right operand.
+
+// Divison should be integer division, e.g eight(dividedBy(three()))/eight(divided_by(three)) should return 2, not 2.666666…
+
+
+var n = function(digit) {
+  return function(op) {
+    return op ? Math.floor(op(digit)) : digit;
+  }
+};
+
+var zero = n(0);
+var one = n(1);
+var two = n(2);
+var three = n(3);
+var four = n(4);
+var five = n(5);
+var six = n(6);
+var seven = n(7);
+var eight = n(8);
+var nine = n(9);
+ 
+function plus(r) { 
+  return function(l) { 
+    return l + r; 
+  }; 
+}
+function minus(r) { 
+  return function(l) { 
+    return l - r; 
+  }; 
+}
+function times(r) { 
+  return function(l) { 
+    return l * r; 
+  }; 
+}
+function dividedBy(r) { 
+  return function(l) { 
+    return l / r; 
+  }; 
+}
