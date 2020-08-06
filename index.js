fd9ba8eaab6a6a74eac2ea3cb8b116a90e2e28c2
wolf-dominion <hannahdruryutd@gmail.com>
2020-08-06 16:28:30 -0600

    Sum Strings, works except for with big numbers

--- index.js
+++ index.js
@@ -183,3 +183,20 @@ for (var key in count) {
   return results;
   }
   
+  // Given the string representations of two integers, 
+  // return the string representation of the sum of those integers.
+  // For example:
+  // sumStrings('1','2') // => '3'
+  // A string representation of an integer will contain no 
+  // characters besides the ten numerals "0" to "9".
+
+  function sumStrings(a,b) { 
+    function parseString(num){
+      if(Number.isNaN(parseInt(num, 10))){
+        return 0
+      } else {
+        return parseInt(num, 10)
+      }
+    }
+    return (BigInt(parseString(a)) + BigInt(parseString(b))).toString()
+  }
