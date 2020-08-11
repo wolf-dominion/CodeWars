656024dfd7e58a107295b0f6f8ba359a85e7b4e8
wolf-dominion <hannahdruryutd@gmail.com>
2020-08-11 14:37:32 -0600

    turn seconds into human readable time format

--- index.js
+++ index.js
@@ -229,3 +229,25 @@ function sumStrings(a, b){
   }
   return sum;
 }
+
+// Write a function, which takes a non-negative integer (seconds) 
+// as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+function humanReadable(num) {
+
+  let hours = num >= 60 ? padNum(Math.floor(num/3600)) : '00';
+  let minutes = num >= 60 ? padNum(Math.floor((num % 3600)/60)) : '00';
+  let seconds = num < 60 ? padNum(num) : num == 60 ? '00' : padNum(Math.floor((num % 3600)/60));
+
+  function padNum(num){
+    return num < 10 ? `0${num}` : `${num}`
+  }
+
+  return `${hours}:${minutes}:${seconds}`
+
+}
